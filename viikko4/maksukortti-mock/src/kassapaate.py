192921ea616c8c738a61f596f0b616aec9204260
Marja Tarvainen <marja.tarvainen@helsinki.fi>
2021-11-22 17:54:06 +0200

    Yksikkötestaus ja riippuvuudet, osat 1 ja 2 (tehtävät 1 ja 2) tehtävä.

---
+++ viikko4/maksukortti-mock/src/kassapaate.py
@@ -0,0 +1,17 @@
+HINTA = 5
+
+
+class Kassapaate:
+    def __init__(self):
+        self.myytyja_lounaita = 0
+
+    def lataa(self, kortti, summa):
+        # Ehto lisätty
+        if summa > 0:
+            kortti.lataa(summa)
+
+    def osta_lounas(self, kortti):
+        # Ehto lisätty
+        if kortti.saldo >= HINTA:
+            kortti.osta(HINTA)
+            self.myytyja_lounaita = self.myytyja_lounaita + 1
